244bf0d204b64886a3ba010e11058fbaa9cf0dd0
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-07-03 22:31:06 +0200

    array methods and loops

--- script.js
+++ script.js
@@ -42,3 +42,61 @@ let greaterFifty = numbers.filter(function(item){
 console.log(greaterFifty);
 
 //1.g
+let divisor = 2;
+function divisibleTwo(array, divisor){
+    return array.filter( number => number % divisor === 0);
+} 
+let dividedNum = divisibleTwo(numbers, divisor);
+console.log(dividedNum);
+
+//1.h
+let divideThree = 3;
+function divisibleThree(array, divideThree){
+    return array.filter( number => number % divisor === 0);
+} 
+let dividedThreeNum = divisibleTwo(numbers, divideThree);
+console.log(dividedThreeNum);
+
+//1.i
+function twoOrThree(array) {
+    return array.filter(number => number % 2 !== 0 && number % 3 !== 0);
+}
+
+let finalResult = twoOrThree(numbers);
+console.log(finalResult);
+
+//1.j
+let countElements = numbers.length;
+console.log( countElements);
+
+//1.k
+let reversedNum = numbers.slice().reverse();
+console.log( reversedNum);
+
+//2.a
+
+let mixedArray = [7, 10, "Clentan", 13, 89, true, 45, false, "Jerry", "Vukona", "Reabetswe", 600];
+let numbersOnly = [];
+
+for (let i = 0; i < mixedArray.length; i++) {
+    if (typeof mixedArray[i] === 'number') {
+        numbersOnly.push(mixedArray[i]);
+    }
+}
+
+console.log("Numbers Only:", numbersOnly);
+
+//2.b
+let stringsOnly = [];
+let i = 0;
+
+while (i < mixedArray.length) {
+    if (typeof mixedArray[i] === 'string') {
+        stringsOnly.push(mixedArray[i]);
+    }
+    i++;
+}
+
+console.log("Strings Only:", stringsOnly);
+
+//2.c
